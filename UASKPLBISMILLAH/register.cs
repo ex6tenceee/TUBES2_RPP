8b0c0f7175964a2711903420617cc306d5ba0573
abdulghofur12 <ghofursans12@gmail.com>
2022-07-03 16:17:46 +0700

    menambahkan metode defensif programing

--- UASKPLBISMILLAH/register.cs
+++ UASKPLBISMILLAH/register.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace UASKPLBISMILLAH
 {
@@ -24,15 +25,21 @@ namespace UASKPLBISMILLAH
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string nama, alamat, username, password;
-            long telp;
+            string nama, telp, alamat, username, password;
              
             nama = textBox1.Text;
-            alamat = textBox2.Text;
-            username = textBox3.Text;
-            password = textBox4.Text;
-            telp = long.Parse(textBox2.Text);
-            MessageBox.Show("nama : " + nama + "\nN.telp:" + telp + "\nAlamat : " + alamat + "\nUsername : " + username + "\nPassword :" + password);
+            telp = textBox2.Text;
+            alamat = textBox3.Text;
+            username = textBox4.Text;
+            password = textBox5.Text;
+            
+
+            Debug.Assert(nama.Length > 0,"nama tidak boleh kosong");
+            Debug.Assert(telp.Length > 0 && telp.Length < 13, "no tidak boleh kosong dan tidak boleh lebih dari 13");
+            Debug.Assert(alamat.Length > 0, "alamat tidak boleh kosong");
+            Debug.Assert(username.Length > 0 && username.Length < 11, "username tidak boleh lebih dari 11");
+            Debug.Assert(password.Length > 0 && password.Length < 10, "password tidak boleh lebih dari 11");
+
 
         }
     }

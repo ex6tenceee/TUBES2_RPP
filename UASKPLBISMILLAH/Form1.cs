d8c07930cf78e98b2da3004d9de539a9cb8b1465
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-07-03 15:08:47 +0700

    Menambahkan logika code untuk melakukan login pada button

--- UASKPLBISMILLAH/Form1.cs
+++ UASKPLBISMILLAH/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace UASKPLBISMILLAH
 {
     public partial class Form1 : Form
@@ -18,11 +22,19 @@ namespace UASKPLBISMILLAH
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            string[] uname = Enum.GetNames(typeof(username));
+            if (password.Contains(textBox2.Text) && uname.Contains(textBox1.Text) && Array.IndexOf(uname, textBox1.Text) == Array.IndexOf(password, textBox2.Text))
+            {
+                label3.Text = "Sukses";
+            }
+            else
+            {
+                label3.Text = "Coba Lagi";
+            }
         }
-
         private void label3_Click(object sender, EventArgs e)
         {
 
         }
     }
+}

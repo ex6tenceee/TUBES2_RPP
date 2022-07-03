36eadd54d5aab6c8d93bb9d4c44fe1f4cb072fce
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-07-03 14:43:18 +0700

    Menambahkan Enum

--- UASKPLBISMILLAH/Form1.cs
+++ UASKPLBISMILLAH/Form1.cs
@@ -7,9 +7,14 @@ namespace UASKPLBISMILLAH
             InitializeComponent();
         }
 
+
+        public enum username { Yapi, Yana, Yani };
+        public string[] password = { "123yapi", "123yana", "123yani" };
+
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
     }
 }

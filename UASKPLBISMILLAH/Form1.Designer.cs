1bc6f42d5793d9e1a95036b3a92af184465508b4
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-07-03 14:39:49 +0700

    Desain Awal

--- UASKPLBISMILLAH/Form1.Designer.cs
+++ UASKPLBISMILLAH/Form1.Designer.cs
@@ -71,7 +71,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(337, 272);
+            this.button1.Location = new System.Drawing.Point(337, 263);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -80,7 +80,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(449, 272);
+            this.button2.Location = new System.Drawing.Point(449, 263);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
